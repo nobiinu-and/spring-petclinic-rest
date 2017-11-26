18b06a8dd3789b0a7f738eb6adfe4d779b76c4b5
Toshiyuki Ando <couger@kt.rim.or.jp>
2017-11-26 13:07:44 +0900

    add application insights for monitoring

--- src/main/java/org/springframework/samples/petclinic/PetClinicApplication.java
+++ src/main/java/org/springframework/samples/petclinic/PetClinicApplication.java
@@ -1,12 +1,32 @@
 package org.springframework.samples.petclinic;
 
+import com.microsoft.applicationinsights.web.internal.WebRequestTrackingFilter;
+import javax.servlet.Filter;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 
 @SpringBootApplication
+@Configuration
 public class PetClinicApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(PetClinicApplication.class, args);
+	}
+
+	@Bean
+	public FilterRegistrationBean someFilterRegistration() {
+		FilterRegistrationBean registration = new FilterRegistrationBean();
+		registration.setFilter(ApplicationInsightsWebFilter());
+		registration.addUrlPatterns("/*");
+		registration.setName("ApplicationInsightsWebFilter");
+		registration.setOrder(1);
+		return registration;
+	}
+
+	public Filter ApplicationInsightsWebFilter() {
+		return new WebRequestTrackingFilter();
 	}
 }
